767517383c1244886cf7cf87010bad30089d3ec4
kekland <kk.erzhan@gmail.com>
2018-11-10 16:57:47 +0600

    Updates

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -13,8 +13,14 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Conference rooms'),
+        title: Text('Conference rooms', style: TextStyle(fontFamily: 'Futura')),
         elevation: 0.0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Container(
         color: Theme.of(context).primaryColor,
